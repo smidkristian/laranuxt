53c240a4640be79b7c675b51be82dab490638762
smidkristian <smid.kristian@gmail.com>
2021-03-26 00:00:38 +0100

    initial

---
+++ frontend/pages/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex justify-center items-center">
+        testing new nuxt app
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
